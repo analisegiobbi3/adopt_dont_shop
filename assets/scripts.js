d9b2809553b6eadd62671b637ee194e3d8780032
analisegiobbi3 <59839160+analisegiobbi3@users.noreply.github.com>
2022-12-15 21:54:43 -0500

    Update scripts.js

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,6 +1,6 @@
 var apiKey = 'qQ7VYpNGk9n2b33tvNje8yNmYVxLsQo7SoYhZ7PPmLYKMhOLC2';
 //remove before commit
-var secretKey = '02v43RmzmoOLmigXxahBZHRRGTBqHerr6NpnVe87';
+var secretKey = '';
 
 $('.dropdown-trigger').dropdown();
 
